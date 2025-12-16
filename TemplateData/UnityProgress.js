e86513e71fbdbd69d78e89deeb28f834dd4ffa27
hinkonboi123 <hinkonboi123@gmail.com>
2025-12-15 21:13:44 -0500

    Update UnityProgress.js

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -30,14 +30,17 @@ function UnityProgress(gameInstance, progress) {
     gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
 
     if(progress>= 0.9 && progress<1)
-    {
-        gameInstance.textProgress.innerHTML = '100% - Running, Wait..' +' <img src="' + rootPath + '/gears.gif" class="spinner" />';
-        gameInstance.progress.style.display = 'none';
-    }
-    else
-    {
-        gameInstance.textProgress.innerHTML = 'Loading - ' + Math.floor(progress * 100) + '%' + ' <img src="' + rootPath + '/gears.gif" class="spinner" />';
-    }
+{
+    gameInstance.textProgress.innerHTML = '100% - Running, Wait..' +
+        ' <img src="https://cdn.jsdelivr.net/gh/hinkonboi123/snowrider3d-webgl@main/TemplateData/gears.gif" class="spinner" />';
+    gameInstance.progress.style.display = 'none';
+}
+else
+{
+    gameInstance.textProgress.innerHTML = 'Loading - ' + Math.floor(progress * 100) + '%' +
+        ' <img src="https://cdn.jsdelivr.net/gh/hinkonboi123/snowrider3d-webgl@main/TemplateData/gears.gif" class="spinner" />';
+}
+
 
     /*
     if (progress == 1) {
